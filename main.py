123abcd91eaef19394244c5dad44cf5412a15253
Guilherme Colatino <AleatorioWorld@gmail.com>
2025-08-20 21:16:52 -0300

    arquivo main

---
+++ main.py
@@ -0,0 +1,9 @@
+import funcoes
+
+funcoes.instanciar("Guilherme", 22, "Estudante")
+funcoes.instanciar("Arthur", 19, "Estudante")
+funcoes.instanciar("Nicoli", 28, "RH")
+funcoes.retornarLista()
+funcoes.editarPessoa("Jeff", 22, "Estudante")
+funcoes.excluirPessoa()
+funcoes.retornarLista()
